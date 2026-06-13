3ab79ecddb1483036a5cdd27b0311824c8e16a71
Rami1l <johnrenonasuncion@gmail.com>
2026-06-14 07:49:17 +0800

    Update FileManagerScreen.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/FileManagerScreen.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/FileManagerScreen.kt
@@ -1,18 +1,59 @@
 package com.movtery.zalithlauncher.ui.screens.content
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+import com.movtery.zalithlauncher.game.path.getGameHome
+
+import java.io.File
 
 @Composable
 fun FileManagerScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val files = remember {
+        File(getGameHome())
+            .listFiles()
+            ?.sortedBy { it.name.lowercase() }
+            ?: emptyList()
+    }
+    
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        Text("Hello File Manager")
+        
+        Text(
+            text = "File Manager",
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        Text(
+            text = getGameHome(),
+            style = MaterialTheme.typography.bodySmall
+        )
+
+        LazyColumn {
+            
+            items(files) { file ->
+
+                Text(
+                    text = if (file.isDirectory) {
+                        "📁 ${file.name}"
+                    } else {
+                        "📄 ${file.name}"
+                    },
+                    modifier = Modifier.padding(vertical = 6.dp)
+                )
+            }
+        }
     }
 }
