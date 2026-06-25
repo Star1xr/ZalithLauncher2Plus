f4a3046efb0d7d1e410727f4e1e73ffa0d159f48
Rami1l <johnrenonasuncion@gmail.com>
2026-06-25 20:33:15 +0800

    [skip ci] Add LegacyControlData - UI data class for legacy layouts

---
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/control/legacy/LegacyControlData.kt
@@ -0,0 +1,12 @@
+package com.movtery.zalithlauncher.game.control.legacy
+
+import java.io.File
+
+data class LegacyControlData(
+    val file: File,
+    val info: LegacyControlInfo,
+    val buttonCount: Int,
+    val drawerCount: Int,
+    val joystickCount: Int,
+    val formatVersion: Int
+)
