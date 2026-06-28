85d11df052ecffeca384b261c92ae90d7e49b795
Rami1l <johnrenonasuncion@gmail.com>
2026-06-28 10:31:09 +0800

    fix: Enable viewBinding for ZL1 backport layouts; add storageProviderAuthorities string

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -123,6 +123,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
+        viewBinding = true
         compose = true
         buildConfig = true
         prefab = true
