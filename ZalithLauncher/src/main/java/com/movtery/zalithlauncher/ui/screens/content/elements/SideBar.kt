ee10c32b30ecac0ccbf8374d7b4529aa8f2b5685
Star1xr <166748405+Star1xr@users.noreply.github.com>
2026-07-24 19:22:27 +0300

    fix: add missing Surface import

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/elements/SideBar.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/elements/SideBar.kt
@@ -48,6 +48,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
